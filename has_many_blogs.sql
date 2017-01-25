043ec55a1abe3f2aced5054ae40255574f97c419
Danika Harada <danimi65@gmail.com>
2017-01-24 23:50:51 -1000

    did up to post table

---
+++ has_many_blogs.sql
@@ -0,0 +1,15 @@
+\c danikaharada;
+
+DROP USER IF EXISTS "has_many_user";
+CREATE USER "has_many_user";
+DROP DATABASE IF EXISTS "has_many_blogs";
+CREATE DATABASE "has_many_blogs" WITH OWNER "has_many_user";
+
+--creating username table
+DROP TABLE IF EXISTS "users";
+CREATE TABLE "users" (id serial PRIMARY KEY NOT NULL, username varchar(90) NOT NULL, first_name varchar(90) DEFAULT NULL, last_name varchar(90) DEFAULT NULL, created_at timestamp WITH TIME ZONE DEFAULT now() NOT NULL, updated_at timestamp WITH TIME ZONE DEFAULT now() NOT NULL);
+
+--creating post table
+DROP TABLE IF EXISTS "posts";
+CREATE TABLE "posts" (id serial PRIMARY KEY
+ NOT NULL, title varchar(180) DEFAULT NULL, url varchar(510) DEFAULT NULL, content text DEFAULT NULL, created_at timestamp WITH TIME ZONE DEFAULT now() NOT NULL, updated_at timestamp WITH TIME ZONE DEFAULT now() NOT NULL);
